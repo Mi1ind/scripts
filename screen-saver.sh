135256b6e69f0fa3767e41f8df64fe92c963e2c5
Milind Sai Waghrey <milindsw6@gmail.com>
2022-11-21 22:21:01 -0500

    update screen saver launcher as raycast script

---
+++ screen-saver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Screen Saver
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌄
+
+# Documentation:
+# @raycast.description Launches the Screen Saver Engine
+# @raycast.author Milidn Sai Waghrey
+# @raycast.authorURL https://twitter.com/milindwaghrey
+
+open /System/Library/CoreServices/ScreenSaverEngine.app
--- screen_saver_launcher.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-open /System/Library/CoreServices/ScreenSaverEngine.app
